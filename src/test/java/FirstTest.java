8e9b4f63b4ad0e6c8b1b3c8b4fbee8524c356434
astaja <asta.jakx@gmail.com>
2023-09-28 14:07:51 +0300

    Updating with 2 tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -5,27 +5,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 public class FirstTest {
-
-    Public static Webdirver driver;
+    public static WebDriver driver;
 
     @Test
-    public void openUrl() {
-        WebDriver driver = new ChromeDriver();
+    public void openUrl1() {
         driver.get("https://www.skelbiu.lt/");
-        driver.findElement(By.id("onetrust-accept-btn-handler")).click(); // agree to Cookies
-
         WebElement searchInput = driver.findElement(By.id("searchKeyword"));
         searchInput.sendKeys("meškiukas");                    // typing custom search keyword
         driver.findElement(By.id("searchButton")).click();                        // click the searh icon in the search bar
-        driver.quit();
-
+    }
+    @Test
+    public void openUrl2() {
+        driver.get("https://www.skelbiu.lt/");
+        WebElement searchInput = driver.findElement(By.id("searchKeyword"));
+        searchInput.sendKeys("keptuvė");                    // typing custom search keyword
+        driver.findElement(By.id("searchButton")).click();                        // click the searh icon in the search bar
     }
 
     @BeforeClass
-    public void BeforeClass () {
+    public void beforeClass () {
+          driver = new ChromeDriver();
+//        driver.get("https://www.skelbiu.lt/");
+//        driver.findElement(By.id("onetrust-accept-btn-handler")).click(); // agree to Cookies
+        acceptCookies();
+    }
+    @AfterClass
+    public void afterClass () {
+     //   driver.quit();
+    }
 
+    public void acceptCookies () {
+        driver = new ChromeDriver();
+        driver.get("https://www.skelbiu.lt/");
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click(); // agree to Cookies
     }
 
 }
